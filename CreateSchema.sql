ea968d147fcf5bcffca8d65f2f7b1b3f649f6372
Samuli Romo <samuli.s.romo@gmail.com>
2020-02-06 21:43:15 +0100

    fixed database schema

--- CreateSchema.sql
+++ CreateSchema.sql
@@ -1,5 +1,8 @@
 ﻿DROP TABLE IF EXISTS Devices, Tasks;
 
+CREATE TYPE IMP AS ENUM ('minor', 'important', 'critical');
+CREATE TYPE TSTATUS AS ENUM ('solved', 'open');
+
 CREATE TABLE Devices( 
   DeviceName VARCHAR PRIMARY KEY,
   DeviceYear INT,
@@ -10,7 +13,7 @@ CREATE TABLE Tasks(
   TaskID SERIAL PRIMARY KEY,
   Submitted TIMESTAMP NOT NULL,
   Device VARCHAR NOT NULL REFERENCES Devices(DeviceName),
-  Importance INT NOT NULL,
+  Importance IMP NOT NULL,
   TaskDescription VARCHAR,
-  TaskStatus INT NOT NULL
+  TaskStatus TSTATUS NOT NULL
 );
